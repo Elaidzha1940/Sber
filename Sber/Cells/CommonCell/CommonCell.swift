c861bd41c83721be2be1a248963de2d3e90d522c
Elaidzha1940 <eli080293@gmail.com>
2024-03-10 15:07:28 +0300

    Added CommonCell

---
+++ Sber/Cells/CommonCell/CommonCell.swift
@@ -0,0 +1,29 @@
+//  /*
+//
+//  Project: Sber
+//  File: CommonCell.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.03.2024
+//
+//  */
+
+import UIKit
+
+class CommonCell: UICollectionViewCell {
+    //MARK: - IBOutlets
+    @IBOutlet private var iconImageView: UIImageView!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var xmarButton: UIButton!
+    
+    //MARK: - Public
+    func configure(icon: UIImage?, text: String) {
+        iconImageView.image = icon
+        textLabel.text = text
+    }
+    
+    //MARK: - View lificycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //
+    }
+}
